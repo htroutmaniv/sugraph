06824acfb3c84b3409a2075b98467e11e1f902c4
htroutmaniv <htroutmaniv@gmail.com>
2025-01-18 18:20:01 -0500

    Front end back end test success. Added app.js initial code

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to Sugraph!</h1>
+      <p>The frontend is working independently for now.</p>
+    </div>
+  );
+};
+
+export default App;
